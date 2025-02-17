c9c50513a642321b3d03b7aa7732878eb3dfc6dd
JimsonR <jimsonjimmy2008@gmail.com>
2025-02-17 20:36:34 +0530

    made changes to ui

--- websocketapp/src/environments/environment.prod.ts
+++ websocketapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    apiUrl: 'http://ec2-13-126-62-213.ap-south-1.compute.amazonaws.com/api',
-    wsUrl: 'ws://ec2-13-126-62-213.ap-south-1.compute.amazonaws.com/ws'
+    apiUrl: 'http://ec2-3-109-54-80.ap-south-1.compute.amazonaws.com/api',
+    wsUrl: 'ws://ec2-3-109-54-80.ap-south-1.compute.amazonaws.com/ws'
 }
