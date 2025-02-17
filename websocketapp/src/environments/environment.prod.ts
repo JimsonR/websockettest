6f74b8bad9c4001f23ef24a06e3b2ee7b263aeb3
JimsonR <jimsonjimmy2008@gmail.com>
2025-02-17 10:37:14 +0530

    commit

--- websocketapp/src/environments/environment.prod.ts
+++ websocketapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    apiUrl: 'http://ec2-15-207-114-112.ap-south-1.compute.amazonaws.com/api',
-    wsUrl: 'ws://ec2-15-207-114-112.ap-south-1.compute.amazonaws.com/ws'
+    apiUrl: 'http://ec2-13-126-62-213.ap-south-1.compute.amazonaws.com/api',
+    wsUrl: 'ws://ec2-13-126-62-213.ap-south-1.compute.amazonaws.com/ws'
 }
