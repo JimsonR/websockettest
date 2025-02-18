c794a888348d3ed2ba62d0956273d7f99a441b31
JimsonR <jimsonjimmy2008@gmail.com>
2025-02-18 11:49:09 +0530

    new instance

--- websocketapp/src/environments/environment.prod.ts
+++ websocketapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    apiUrl: 'http://ec2-3-109-54-80.ap-south-1.compute.amazonaws.com/api',
-    wsUrl: 'ws://ec2-3-109-54-80.ap-south-1.compute.amazonaws.com/ws'
+    apiUrl: 'http://eec2-15-206-128-95.ap-south-1.compute.amazonaws.com/api',
+    wsUrl: 'ws://ec2-15-206-128-95.ap-south-1.compute.amazonaws.com/ws'
 }
